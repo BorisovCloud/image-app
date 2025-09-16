dafd45701cd6e2bc38f3ba8780c170fdc265b26a
Anton Borisov <anton@borisov.cloud>
2025-09-16 20:54:15 +0300

    Add App Service Plan and Linux Web App resources with ACR integration

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -87,3 +87,44 @@ resource "azurerm_container_registry" "main" {
   sku                 = "Basic"
 }
 
+# Create App Service Plan
+resource "azurerm_service_plan" "main" {
+  name                = "asp-${var.project_name}-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+  tags                = var.tags
+}
+
+# Assign AcrPull role to the managed identity for ACR
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.main.principal_id
+}
+
+# Create Linux Web App with Docker container
+resource "azurerm_linux_web_app" "main" {
+  name                = "app-${var.project_name}-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+  tags                = var.tags
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.main.id]
+  }
+
+  site_config {
+    application_stack {
+      docker_registry_url      = "https://${azurerm_container_registry.main.login_server}"
+      docker_image_name        = "${var.project_name}:latest"
+    }
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+}
