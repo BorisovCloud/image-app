857d1a971706505ee38d2065f2e7219aabccd897
Anton Borisov <anton@borisov.cloud>
2025-09-16 20:24:46 +0300

    Add Azure Container Registry resource to Terraform configuration

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -79,3 +79,11 @@ resource "azurerm_role_assignment" "storage_account_contributor" {
   role_definition_name = "Storage Account Contributor"
   principal_id         = azurerm_user_assigned_identity.main.principal_id
 }
+
+resource "azurerm_container_registry" "main" {
+  name                = "acr${var.project_name}${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Basic"
+}
+
