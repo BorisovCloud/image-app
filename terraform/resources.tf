b86dc64192db6399652ff32b0c6e6c339544150f
Anton Borisov <anton@borisov.cloud>
2025-09-16 19:30:58 +0300

    Fix container registry name formatting in Terraform configuration

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -76,7 +76,7 @@ resource "azurerm_user_assigned_identity" "main" {
 }
 
 resource "azurerm_container_registry" "main" {
-  name                = "acr-${var.project_name}-${var.environment}"
+  name                = "acr${var.project_name}${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "Basic"
